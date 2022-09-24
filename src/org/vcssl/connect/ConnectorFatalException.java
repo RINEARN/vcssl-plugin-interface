8da0ba0f0e59a0a7888d246389f68207280057ef
RINEARN <commit@rinearn.com>
2022-09-24 12:42:55 +0900

    Add: ConnectorFatalException

---
+++ src/org/vcssl/connect/ConnectorFatalException.java
@@ -0,0 +1,58 @@
+/*
+ * ==================================================
+ * Connector Fatal Exception
+ * --------------------------------------------------
+ * This file is released under CC0.
+ * Written in 2022 by RINEARN
+ * ==================================================
+ */
+
+// THE STATUS OF THE SPECIFICATION OF THIS CLASS IS "EXTENSION ONLY".
+// IN PRINCIPLE, DON'T DELETE EXISTING CONSTRUCTORS/METHODS/FIELDS.
+
+package org.vcssl.connect;
+
+
+/**
+ * An Exception class thrown when errors have occurred, 
+ * caused by incorrect implementations (might be bugs) of plug-ins or scripting engines.
+ */
+@SuppressWarnings("serial")
+public class ConnectorFatalException extends RuntimeException {
+
+	/**
+	 * Creates a ConnectorFatalException having no error message.
+	 */
+	public ConnectorFatalException() {
+		super();
+	}
+
+	/**
+	 * Creates a ConnectorFatalException having the specified error message.
+	 * 
+	 * @param errorMessage The error message.
+	 */
+	public ConnectorFatalException(String errorMessage) {
+		super(errorMessage);
+	}
+
+	/**
+	 * Creates a ConnectorFatalException wrapping the specified Throwable.
+	 * 
+	 * @param wrappedThrowable The throwable of the cause, to be wrapped by the created ConnectorFatalException.
+	 */
+	public ConnectorFatalException(Throwable wrappedThrowable) {
+		super(wrappedThrowable);
+	}
+
+	/**
+	 * Creates a ConnectorFatalException having the specified error message, and wrapping the specified Throwable.
+	 * 
+	 * @param errorMessage The error message.
+	 * @param wrappedThrowable The throwable of the cause, to be wrapped by the created ConnectorFatalException.
+	 */
+	public ConnectorFatalException(String errorMessage, Throwable wrappedThrowable) {
+		super(errorMessage, wrappedThrowable);
+	}
+}
+
