7b26e70040a1a1ca74506686a4d31f3d14914291
RINEARN <commit@rinearn.com>
2022-09-24 15:40:36 +0900

    Refactor: XSCI1: Clean-up comments

--- src/org/vcssl/connect/ExternalStructConnectorInterface1.java
+++ src/org/vcssl/connect/ExternalStructConnectorInterface1.java
@@ -1,25 +1,16 @@
 /*
  * ==================================================
  * External Struct Connector Interface 1 (XSCI 1)
- * 
- * Online Reference: https://www.vcssl.org/en-us/plugin/jimpl/ref/org/vcssl/connect/ExternalStructConnectorInterface1.html
- * オンライン仕様書: https://www.vcssl.org/ja-jp/plugin/jimpl/ref/org/vcssl/connect/ExternalStructConnectorInterface1.html
  * --------------------------------------------------
  * This file is released under CC0.
- * Written in 2021-2022 by RINEARN (Fumihiro Matsui)
+ * Written in 2021-2022 by RINEARN
  * ==================================================
  */
 
  package org.vcssl.connect;
 
 /**
- * <span class="lang-en">
- * An empty interface, defined for expanding features of XNCI1 interface in the future
- * </span>
- * <span class="lang-ja">
- * XNCI1 インターフェースの将来的な拡張のために定義されている, 現状では空のインターフェースです
- * </span>
- * .
+ * An empty interface, defined for expanding features of XNCI1 interface in the future.
  */
 public class ExternalStructConnectorInterface1 {
 
