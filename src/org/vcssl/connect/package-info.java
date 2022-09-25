009c889cfe9e4dab64996a1026f90b9cbaabed83
RINEARN <commit@rinearn.com>
2022-09-26 03:47:25 +0900

    Refactor: package-info: clean-up comments

--- src/org/vcssl/connect/package-info.java
+++ src/org/vcssl/connect/package-info.java
@@ -1,21 +1,12 @@
 /*
  * ==================================================
  * VCSSL/Vnano Plug-in Interface Package
- * 
- * Online Reference: https://www.vcssl.org/en-us/plugin/jimpl/ref/org/vcssl/connect/
- * オンライン仕様書: https://www.vcssl.org/ja-jp/plugin/jimpl/ref/org/vcssl/connect/
  * --------------------------------------------------
  * This package is released under CC0.
  * ==================================================
  */
 
 /**
- * <span class="lang-en">
- * The package providing interfaces to implement plug-ins for VCSSL/Vnano script engines
- * </span>
- * <span class="lang-ja">
- * VCSSL/Vnanoスクリプトエンジンに対するプラグイン実装用インターフェースを提供するパッケージです
- * </span>
- * .
+ * The package providing interfaces to implement plug-ins for VCSSL/Vnano scripting engines.
  */
 package org.vcssl.connect;
